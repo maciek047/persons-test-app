c5ba54e64a227d3bcba1e3e96e9418d862bfc87d
Maciej <m.szymeczko@dac.digital>
2023-07-18 13:46:54 +0200

    changed config

--- src/main/java/plan3/recruitment/backend/PersonDirectoryService.java
+++ src/main/java/plan3/recruitment/backend/PersonDirectoryService.java
@@ -2,6 +2,8 @@ package plan3.recruitment.backend;
 
 import io.dropwizard.Application;
 import io.dropwizard.Configuration;
+import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
+import io.dropwizard.configuration.SubstitutingSourceProvider;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import plan3.recruitment.backend.resources.PersonResource;
@@ -10,6 +12,12 @@ public class PersonDirectoryService extends Application<Configuration> {
 
     @Override
     public void initialize(final Bootstrap<Configuration> bootstrap) {
+        bootstrap.setConfigurationSourceProvider(
+                new SubstitutingSourceProvider(bootstrap.getConfigurationSourceProvider(),
+                        new EnvironmentVariableSubstitutor(false)
+                )
+        );
+
     }
 
     public static void main(final String[] args) throws Exception {
